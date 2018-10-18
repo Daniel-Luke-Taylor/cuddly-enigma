2dfce63c1bedd70208f6f707ba7e11b71ede9375
Daniel Luke Taylor <32296705+Daniel-Luke-Taylor@users.noreply.github.com>
2018-10-18 20:41:00 +0100

    Unity WebGL Test
    -Created Unity project for microgames
    -Created test scene (can test if orb disappears with space)
    -Compiled project to test (open index)
    
    Set up a unity webgl project and tested the compiling and creation on webgl projects.

---
+++ MicroGames/Assets/spacetogone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spacetogone : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            print("jump");
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            print("unjump");
+            gameObject.GetComponent<MeshRenderer>().enabled = true;
+        }
+    }
+}
