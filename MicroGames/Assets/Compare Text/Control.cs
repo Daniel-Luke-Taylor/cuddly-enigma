a3cded7fd20a56b378582d4419df6651c39e110d
Daniel Luke Taylor <32296705+Daniel-Luke-Taylor@users.noreply.github.com>
2018-11-22 12:40:51 +0000

    Compare Text - Select text
    -Created scene for Compare Documents
    -Created selection mechanic for use in both games
    -Started work on completion of Compare Documents

---
+++ MicroGames/Assets/Compare Text/Control.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+
+    private int correct;
+    public int pointsNeeded;
+
+	public void UpdateProgess()
+    {
+        correct++;
+        if(correct <= pointsNeeded)
+        {
+            Complete();
+        }
+    }
+
+    private void Complete()
+    {
+        print("correct");
+    }
+}
---
+++ MicroGames/Assets/Compare Text/Correct.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Correct : MonoBehaviour {
+
+    public Image ring;
+    public float fillSpeed = 1f;
+    public bool clicked;
+
+    public void Clicked()
+    {
+        clicked = true;
+    }
+    private void Update()
+    {
+        if (clicked)
+        {
+            ring.fillAmount = ring.fillAmount + (Time.deltaTime * fillSpeed);
+        }
+    }
+
+}
