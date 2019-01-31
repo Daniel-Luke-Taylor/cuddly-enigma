87949c3541cc81cabae40ba8bc6760c052454f41
Daniel Luke Taylor <32296705+Daniel-Luke-Taylor@users.noreply.github.com>
2019-01-31 15:40:58 +0000

    Microgames load
    -Specific Microgames will now load based on url (lockpick,document,statement,decoder and photo)

--- MicroGames/Assets/LoadScene.cs
+++ MicroGames/Assets/LoadScene.cs
@@ -5,7 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour {
 
-	public void LoadSceneName(string name)
+    private void Start()
+    {
+        //get currentURL
+        string currentURL = Application.absoluteURL;
+        //currentURL = "file:///C:/Users/Daniel%20Taylor/Desktop/Digital%20Artifact%20and%20Demo/cuddly-enigma/docs/lockpick.html";
+        //Check the URL for specific strings and open scenes
+        if (currentURL.Contains("lockpick"))
+        {
+            LoadSceneName("PickLock");
+        }
+        else if (currentURL.Contains("photo"))
+        {
+            LoadSceneName("Photograph");
+        }
+        else if (currentURL.Contains("document"))
+        {
+            LoadSceneName("Compare Documents");
+        }
+        else if (currentURL.Contains("statement"))
+        {
+            LoadSceneName("Compare Statements");
+        }
+        else if (currentURL.Contains("decoder"))
+        {
+            LoadSceneName("Decoder");
+        }
+    }
+
+    public void LoadSceneName(string name)
     {
         SceneManager.LoadScene(name);
     }
