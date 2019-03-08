7b2a1fa882d9bd30a053177d1484cbf482bdbf49
Daniel Luke Taylor <32296705+Daniel-Luke-Taylor@users.noreply.github.com>
2019-03-08 12:00:08 +0000

    update

--- MicroGames/Assets/Pick Lock/Lock.cs
+++ MicroGames/Assets/Pick Lock/Lock.cs
@@ -7,6 +7,7 @@ public class Lock : MonoBehaviour {
     public float unlockRange;
     public Slider lockSlider;
     private float unlockPoint;
+    public Transform spinLock;
     
 
 	// Use this for initialization
@@ -24,6 +25,11 @@ public class Lock : MonoBehaviour {
             Unlock();
             gameObject.GetComponent<WinGame>().Win();
         }
+    }
+
+    public void UpdateRotate()
+    {
+        spinLock.Rotate(0, 0, lockSlider.value);
     }
 
     private void Unlock()
